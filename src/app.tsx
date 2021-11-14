9c6566db32baa71e6fd204b815f061a87a5d384a
Alexandr Kislyakov <kislyakov.sk@yandex.ru>
2021-11-15 02:15:02 +0300

    append css in js `emotion`

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,10 @@
-const App = () => <h1>Hello world</h1>;
+import { css } from '@emotion/react';
+
+const titleStyle = css({
+  color: 'red',
+  margin: 0,
+})
+
+const App = () => <h1 css={titleStyle}>Hello world</h1>;
 
 export { App }
