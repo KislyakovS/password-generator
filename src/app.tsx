96a0a0248c86a9ec5453ea85283bdd9002ed9c57
Alexandr Kislyakov <kislyakov.sk@yandex.ru>
2021-11-15 02:45:58 +0300

    append eslint and prettier

--- src/app.tsx
+++ src/app.tsx
@@ -3,8 +3,8 @@ import { css } from '@emotion/react';
 const titleStyle = css({
   color: 'red',
   margin: 0,
-})
+});
 
 const App = () => <h1 css={titleStyle}>Hello world</h1>;
 
-export { App }
+export { App };
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
-import ReactDOM from "react-dom";
+import ReactDOM from 'react-dom';
 import { App } from './app';
 
-const app = document.getElementById("app");
+const app = document.getElementById('app');
 ReactDOM.render(<App />, app);
