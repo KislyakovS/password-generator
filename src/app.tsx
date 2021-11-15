65460126e0ec9e2ab257046c7c04e1f3530912ce
Alexandr Kislyakov <kislyakov.sk@yandex.ru>
2021-11-16 02:29:14 +0300

    remove emotion from components

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,3 @@
-import { css } from '@emotion/react';
-
-const titleStyle = css({
-  color: 'red',
-  margin: 0,
-});
-
-const App = () => <h1 css={titleStyle}>Hello world</h1>;
+const App = () => <h1>Hello world</h1>;
 
 export { App };
