7dfc4245684de85bbb400049da57671f97385e93
Alexandr Kislyakov <kislyakov.sk@yandex.ru>
2021-11-17 18:17:46 +0300

    append component `InputPassword`

---
+++ src/components/input-password/index.tsx
@@ -0,0 +1,39 @@
+// Core
+import { FC } from 'react';
+
+// Icons
+import LockOpenIcon from './icons/lock-open.svg';
+import CopyIcon from './icons/copy.svg';
+import RefreshIcon from './icons/refresh.svg';
+
+// Instruments
+import * as classes from './input-password.module.css';
+
+const InputPassword: FC = () => {
+  return (
+    <div className={classes.container}>
+      <div className={classes.lock}>
+        <LockOpenIcon />
+      </div>
+      <input className={classes.input} value="TpPG%Vhlk2*k" />
+      <div className={classes.actions}>
+        <button>
+          <CopyIcon />
+        </button>
+        <button>
+          <RefreshIcon />
+        </button>
+      </div>
+      <ul className={classes.complexity}>
+        <li className={classes.active}></li>
+        <li className={classes.active}></li>
+        <li className={classes.active}></li>
+        <li className={classes.active}></li>
+        <li className={classes.active}></li>
+        <li className={classes.active}></li>
+      </ul>
+    </div>
+  )
+};
+
+export { InputPassword }
