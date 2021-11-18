3492a27dd4ad9708aba08d9be3069d5a0012b4f6
Alexandr Kislyakov <kislyakov.sk@yandex.ru>
2021-11-18 21:38:13 +0300

    append ui component `Range`

---
+++ src/ui/range/thumb.tsx
@@ -0,0 +1,18 @@
+// Core
+import { FC } from 'react';
+
+// Instruments
+import * as classes from './thumb.module.css';
+
+// Types
+import { IThumbProps } from 'react-range/lib/types';
+
+type ThumbType = {
+  props: IThumbProps,
+}
+
+const Thumb: FC<ThumbType> = ({ props }: ThumbType) => {
+  return <div {...props} className={classes.slider} />;
+}
+
+export { Thumb };
