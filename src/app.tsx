622de9ddafb2e2ef9b4de9cb451f2720a983e2a8
Alexandr Kislyakov <kislyakov.sk@yandex.ru>
2021-11-20 17:22:03 +0300

    integration

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,35 @@
+// Components
 import { InputPassword } from './components/input-password';
 import { Customize } from './components/customize';
 
-const App = () => (
-  <>
-    <InputPassword />
-    <Customize />
-  </>
-);
+// Hooks
+import { useCustomize } from './bus/customize/hooks';
+import { usePassword } from './bus/password/hooks';
+
+const App = () => {
+  const { password, setup, generate } = usePassword();
+  const {
+    options,
+    setLength,
+    setIsUppercase,
+    setIsLowercase,
+    setIsSymbol,
+    setIsNumber
+  } = useCustomize(setup, generate);
+
+  return (
+    <>
+      <InputPassword password={password} onUpdate={generate} />
+      <Customize
+        options={options}
+        setLength={setLength}
+        setIsUppercase={setIsUppercase}
+        setIsLowercase={setIsLowercase}
+        setIsSymbol={setIsSymbol}
+        setIsNumber={setIsNumber}
+      />
+    </>
+  )
+}
 
 export { App };
