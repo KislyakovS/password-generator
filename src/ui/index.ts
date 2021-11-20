e9d9549b1271aad26e4a71dbbcacb07f62560aa4
Alexandr Kislyakov <kislyakov.sk@yandex.ru>
2021-11-20 18:53:21 +0300

    append ui component `Section`

--- src/ui/index.ts
+++ src/ui/index.ts
@@ -2,3 +2,4 @@ export { Checkbox } from './checkbox';
 export { Input } from './input';
 export { Range } from './range';
 export { Logo } from './logo';
+export { Section } from './section';
---
+++ src/ui/section/index.tsx
@@ -0,0 +1,24 @@
+// Core
+import { FC, ReactNode } from 'react';
+
+// Styles
+import * as classes from './section.module.css';
+
+// Types
+type SectionPropsType = {
+  title: string,
+  description?: string,
+  children: ReactNode
+}
+
+const Section: FC<SectionPropsType> = ({ title, description, children }) => (
+  <section className={classes.section}>
+    <h3 className={classes.title}>{title}</h3>
+    {description && <p className={classes.description}>{description}</p>}
+    <div className={classes.body}>
+      {children}
+    </div>
+  </section>
+);
+
+export { Section }
