244a1f3b8d3cdc878f258ec6558a200ccbd4b8ba
Alexandr Kislyakov <kislyakov.sk@yandex.ru>
2021-11-21 17:53:41 +0300

    optional props `marks`

--- src/ui/range/index.tsx
+++ src/ui/range/index.tsx
@@ -14,7 +14,7 @@ export type RangeType = {
   step?: number,
   min: number,
   max: number,
-  marks: string[],
+  marks?: string[],
   onChange: (value: number) => void,
 }
 
@@ -27,7 +27,7 @@ const Range: FC<RangeType> = ({ className, step = 1, min, max, value, marks, onC
       max={max}
       onChange={([value]) => onChange(value)}
       renderTrack={track(min, max, value)}
-      renderMark={mark(marks, value, step)}
+      renderMark={marks ? mark(marks, value, step) : undefined}
       renderThumb={Thumb}
     />
   </div>
