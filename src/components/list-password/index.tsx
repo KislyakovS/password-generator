63b833ebd1591936d31fbcadac3f1128ff4b880f
Alexandr Kislyakov <kislyakov.sk@yandex.ru>
2021-11-21 17:55:13 +0300

    fix component `ListPassword`

--- src/components/list-password/index.tsx
+++ src/components/list-password/index.tsx
@@ -4,7 +4,6 @@ import { FC } from 'react';
 // Styles
 import * as classes from './list-password.module.css';
 
-
 const ListPassword: FC = () => (
   <ul className={classes.list}>
     {["eQe@8ZZ&Qbuv$Kcd", "SD?w7LD_EW98zR-r", "MhEQ79hJx_^#$^SQ", "98@Nz$SNky=4UU3v"].map(password => (
