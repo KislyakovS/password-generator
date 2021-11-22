1b7dd205dafb37b51c0b1a9a96207fdf389f55eb
Alexandr Kislyakov <kislyakov.sk@yandex.ru>
2021-11-22 17:38:03 +0300

    remove bus

--- src/app.tsx
+++ src/app.tsx
@@ -2,21 +2,7 @@
 import { Logo } from './ui';
 import { FormGenerate, ListPassword } from './components';
 
-// Hooks
-import { useCustomize } from './bus/customize/hooks';
-import { usePassword } from './bus/password/hooks';
-
 const App = () => {
-  const { password, setup, generate } = usePassword();
-  const {
-    options,
-    setLength,
-    setIsUppercase,
-    setIsLowercase,
-    setIsSymbol,
-    setIsNumber
-  } = useCustomize(setup, generate);
-
   return (
     <>
       <Logo />
