bc901730a6c102762833662f1f7ab20b9ee073d1
Alexandr Kislyakov <kislyakov.sk@yandex.ru>
2022-01-30 15:49:06 +0300

    test ui component `Button`

--- __tests__/ui/button.spec.tsx
+++ __tests__/ui/button.spec.tsx
@@ -1,6 +1,6 @@
 // Core
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 // Components
 import { Button } from '../../src/ui';
@@ -8,9 +8,22 @@ import { Button } from '../../src/ui';
 describe('Component Button', () => {
   it('should be text in the button "Button 1"', () => {
     const CHILDREN_TEXT = 'Button 1';
-    const { getByText } = render(<Button>{CHILDREN_TEXT}</Button>);
-    const element = getByText(CHILDREN_TEXT);
 
-    expect(element).toBeInTheDocument();
+    const { getByRole } = render(<Button>{CHILDREN_TEXT}</Button>);
+    const button = getByRole('button');
+
+    expect(button).toHaveTextContent(CHILDREN_TEXT);
   });
+
+  it('should call for a click on the button', () => {
+    const onClick = jest.fn();
+
+    const { getByRole } = render(<Button onClick={onClick}>Button</Button>);
+    const button = getByRole('button');
+
+    fireEvent.click(button);
+
+    expect(onClick).toHaveBeenCalled();
+  })
 });
+
