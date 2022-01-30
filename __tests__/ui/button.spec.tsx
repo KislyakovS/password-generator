da69b7c82047cb749522fc3377dc3a41425cab0f
Alexandr Kislyakov <kislyakov.sk@yandex.ru>
2022-01-30 15:04:18 +0300

    setup tests

--- __tests__/ui/button.spec.tsx
+++ __tests__/ui/button.spec.tsx
@@ -1,49 +1,16 @@
 // Core
 import React from 'react';
-import { render, unmountComponentAtNode } from 'react-dom';
-import { act } from 'react-dom/test-utils';
+import { render } from '@testing-library/react';
 
 // Components
 import { Button } from '../../src/ui';
 
 describe('Component Button', () => {
-  let container: Element;
-
-  beforeEach(() => {
-    container = document.createElement('div');
-    document.body.appendChild(container);
-  });
-
-  afterEach(() => {
-    unmountComponentAtNode(container);
-    container.remove();
-  });
-
   it('should be text in the button "Button 1"', () => {
     const CHILDREN_TEXT = 'Button 1';
+    const { getByText } = render(<Button>{CHILDREN_TEXT}</Button>);
+    const element = getByText(CHILDREN_TEXT);
 
-    act(() => {
-      render(<Button>{CHILDREN_TEXT}</Button>, container);
-    });
-
-    expect(container.textContent).toBe(CHILDREN_TEXT);
-  });
-
-  it('should button click triggered', () => {
-    const onClick = jest.fn();
-
-    act(() => {
-      render(<Button onClick={onClick}>onClick</Button>, container);
-    });
-
-    const button = container.querySelector('button');
-
-    act(() => {
-      for (let i = 0; i < 5; i++) {
-        button?.dispatchEvent(new MouseEvent('click', { bubbles: true }));
-      }
-    });
-
-    expect(onClick).toHaveBeenCalledTimes(5);
+    expect(element).toBeInTheDocument();
   });
 });
