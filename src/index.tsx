71c07c492085eeaa13130a67395816e4c759f1c3
Alexandr Kislyakov <kislyakov.sk@yandex.ru>
2022-04-01 19:44:47 +0300

    added registration service-worker

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,10 @@
 import ReactDOM from 'react-dom';
 import { App } from './app';
+import * as serviceWorkerRegistration from './service-worker-registration';
 
 import './global.css';
 
 const app = document.getElementById('app');
 ReactDOM.render(<App />, app);
+
+serviceWorkerRegistration.register();
