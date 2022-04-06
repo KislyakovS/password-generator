9ecfc38c9592eb15fccb6d33531091f536257719
Alexandr Kislyakov <kislyakov.sk@yandex.ru>
2022-04-06 22:53:35 +0300

    added `it` for `Input` check `label`

--- __tests__/ui/input.spec.tsx
+++ __tests__/ui/input.spec.tsx
@@ -9,7 +9,7 @@ describe('Component Input', () => {
   it('should input have value "Value 1"', () => {
     const INPUT_VALUE = 'Value 1';
 
-    const { getByRole } = render(<Input value={INPUT_VALUE} readOnly />);
+    const { getByRole } = render(<Input label="" value={INPUT_VALUE} readOnly />);
     const element = getByRole('textbox');
 
     expect(element).toHaveValue(INPUT_VALUE);
@@ -19,7 +19,7 @@ describe('Component Input', () => {
     const onChange = jest.fn();
     const INPUT_VALUE = 'Input';
 
-    const { getByRole } = render(<Input onChange={onChange} />);
+    const { getByRole } = render(<Input label="" onChange={onChange} />);
     const element = getByRole('textbox');
 
     fireEvent.change(element, { target: { value: INPUT_VALUE } });
@@ -27,5 +27,13 @@ describe('Component Input', () => {
     expect(element).toHaveValue(INPUT_VALUE);
     expect(onChange).toHaveBeenCalled();
   });
-});
 
+  it('should label have value "label 1"', () => {
+    const LABEL_TEXT = 'label 1';
+
+    const { getByText } = render(<Input label={LABEL_TEXT} readOnly />);
+    const label = getByText(LABEL_TEXT);
+
+    expect(label.textContent).toBe(LABEL_TEXT);
+  });
+});
