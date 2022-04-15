76c29c263bacf58eea806c2bbd340839bb4ae10c
Alexandr Kislyakov <kislyakov.sk@yandex.ru>
2022-04-15 13:31:43 +0300

    added utils func `Storage` and `ThemeStorage`

---
+++ src/utils/theme-storage.ts
@@ -0,0 +1,18 @@
+import { Storage } from "./storage";
+
+enum Theme {
+  dark = 'dark',
+  light = 'light',
+}
+
+class ThemeStorage extends Storage<'theme', Theme> {
+  getTheme() {
+    return this.get('theme');
+  }
+
+  setTheme(value: Theme) {
+    this.set('theme', value);
+  }
+}
+
+export { Theme, ThemeStorage }
