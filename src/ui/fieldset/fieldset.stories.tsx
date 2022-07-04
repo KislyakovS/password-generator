c0a1dff1cb848d5ba71352f0e5f505b42aa0c44e
Alexander Kislyakov <kislyakov.sk@yandex.ru>
2022-07-04 03:36:44 +0300

    update

--- src/ui/fieldset/fieldset.stories.tsx
+++ src/ui/fieldset/fieldset.stories.tsx
@@ -6,6 +6,10 @@ import { Fieldset } from './index';
 export default {
   title: 'UI/Fieldset',
   component: Fieldset,
+  args: {
+    legend: 'Legend',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+  },
 } as ComponentMeta<typeof Fieldset>;
 
 // eslint-disable-next-line max-len
