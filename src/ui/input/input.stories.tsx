2670246a85236535a555d05b7f265b7a63e1f1e9
Alexander Kislyakov <kislyakov.sk@yandex.ru>
2022-07-06 20:59:51 +0300

    feat: add 2 stories

--- src/ui/input/input.stories.tsx
+++ src/ui/input/input.stories.tsx
@@ -6,7 +6,17 @@ import { Input } from './index';
 export default {
   title: 'UI/Input',
   component: Input,
+  args: {
+    label: 'Email: ',
+    placeholder: 'Email',
+    type: 'email',
+  },
 } as ComponentMeta<typeof Input>;
 
 // eslint-disable-next-line max-len
 export const Template: ComponentStory<typeof Input> = (args) => <Input {...args} />;
+
+export const HiddelLabel = Template.bind({});
+HiddelLabel.args = {
+  isHiddenLabel: true,
+};
--- src/ui/switcher/switcher.stories.tsx
+++ src/ui/switcher/switcher.stories.tsx
@@ -6,7 +6,15 @@ import { Switcher } from './index';
 export default {
   title: 'UI/Switcher',
   component: Switcher,
+  args: {
+    checked: false,
+  },
 } as ComponentMeta<typeof Switcher>;
 
 // eslint-disable-next-line max-len
 export const Template: ComponentStory<typeof Switcher> = (args) => <Switcher {...args}>Switcher</Switcher>;
+
+export const Checked = Template.bind({});
+Checked.args = {
+  checked: true,
+};
