ac0fe3cdb4546a2334d644f36762f70dd3577d15
Alexander Kislyakov <kislyakov.sk@yandex.ru>
2022-07-16 19:25:14 +0300

    fix: test for `password-generator`

--- __tests__/utils/password-generator.spec.ts
+++ __tests__/utils/password-generator.spec.ts
@@ -1,90 +1,104 @@
 import { PasswordGenerator } from '../../src/utils';
 
-describe('When using the password generator, it:', () => {
-  let password = new PasswordGenerator();
+describe('PasswordGenerator', () => {
+  let sut: PasswordGenerator;
 
   beforeEach(() => {
-    password = new PasswordGenerator();
+    sut = new PasswordGenerator();
   });
 
-  it('should generate a password of default length 12', () => {
-    expect(password.generate().length).toBe(12);
-  });
+  describe('setup', () => {
+    it('should generate a password of default length 12', () => {
+      const result = sut.generate();
 
-  it('should set a password length of 20', () => {
-    const LENGTH_PASSWORD = 20;
-
-    password.setup({ length: LENGTH_PASSWORD });
-
-    expect(password.generate().length).toBe(LENGTH_PASSWORD);
-  });
-
-  it('should give an error when transmitting zero length', () => {
-    const LENGTH_PASSWORD = 0;
-
-    password.setup({ length: LENGTH_PASSWORD });
-
-    expect(password.generate).toThrowError();
-  });
-
-  it('should give an error if there are no options', () => {
-    password.setup({
-      isNumber: false, isSymbol: false, isUppercase: false, isLowercase: false,
+      expect(result).toHaveLength(12);
     });
 
-    expect(password.generate).toThrowError();
-  });
+    it('should set a password length of 20', () => {
+      const LENGTH_PASSWORD = 20;
+      sut.setup({ length: LENGTH_PASSWORD });
 
-  it('should password must be generated from numbers only', () => {
-    password.setup({
-      isNumber: true, isSymbol: false, isUppercase: false, isLowercase: false,
+      const result = sut.generate();
+
+      expect(result).toHaveLength(LENGTH_PASSWORD);
     });
 
-    expect(password.generate()).toEqual(expect.stringMatching(/[0-9]+/));
-  });
+    it('should give an error when transmitting zero length', () => {
+      const LENGTH_PASSWORD = 0;
+      sut.setup({ length: LENGTH_PASSWORD });
 
-  it('should password must be generated from symbol only', () => {
-    password.setup({
-      isNumber: false, isSymbol: true, isUppercase: false, isLowercase: false,
+      expect(sut.generate).toThrowError();
     });
 
-    expect(password.generate()).toEqual(expect.stringMatching(/[!@#$%^&*()+_\-=}{[\]|:;"/?.><,`~]+/));
-  });
+    it('should give an error if there are no options', () => {
+      sut.setup({
+        isNumber: false, isSymbol: false, isUppercase: false, isLowercase: false,
+      });
 
-  it('should password must be generated from uppercase only', () => {
-    password.setup({
-      isNumber: false, isSymbol: false, isUppercase: true, isLowercase: false,
+      expect(sut.generate).toThrowError();
     });
 
-    expect(password.generate()).toEqual(expect.stringMatching(/[A-Z]+/));
-  });
+    it('should password must be generated from numbers only', () => {
+      sut.setup({
+        isNumber: true, isSymbol: false, isUppercase: false, isLowercase: false,
+      });
 
-  it('should password must be generated from lowercase only', () => {
-    password.setup({
-      isNumber: false, isSymbol: false, isUppercase: false, isLowercase: true,
+      const result = sut.generate();
+
+      expect(result).toMatch(/[0-9]{12}/);
     });
 
-    expect(password.generate()).toEqual(expect.stringMatching(/[a-z]+/));
-  });
+    it('should password must be generated from symbol only', () => {
+      sut.setup({
+        isNumber: false, isSymbol: true, isUppercase: false, isLowercase: false,
+      });
 
-  it('matches even if received contains additional elements', () => {
-    password.setup({
-      isNumber: true, isSymbol: true, isUppercase: true, isLowercase: true, isStrict: true,
+      const result = sut.generate();
+
+      expect(result).toMatch(/[!@#$%^&*()+_\-=}{[\]|:;"/?.><,`~]{12}/);
     });
 
-    const passwordStr = password.generate();
+    it('should password must be generated from uppercase only', () => {
+      sut.setup({
+        isNumber: false, isSymbol: false, isUppercase: true, isLowercase: false,
+      });
 
-    expect(passwordStr).toMatch(/[0-9]/);
-    expect(passwordStr).toMatch(/[!@#$%^&*()+_\-=}{[\]|:;"/?.><,`~]/);
-    expect(passwordStr).toMatch(/[A-Z]/);
-    expect(passwordStr).toMatch(/[a-z]/);
+      const result = sut.generate();
+
+      expect(result).toMatch(/[A-Z]{12}/);
+    });
+
+    it('should password must be generated from lowercase only', () => {
+      sut.setup({
+        isNumber: false, isSymbol: false, isUppercase: false, isLowercase: true,
+      });
+
+      const result = sut.generate();
+
+      expect(result).toMatch(/[a-z]{12}/);
+    });
+
+    it('matches even if received contains additional elements', () => {
+      sut.setup({
+        isNumber: true, isSymbol: true, isUppercase: true, isLowercase: true, isStrict: true,
+      });
+
+      const result = sut.generate();
+
+      expect(result).toMatch(/[0-9]/);
+      expect(result).toMatch(/[!@#$%^&*()+_\-=}{[\]|:;"/?.><,`~]/);
+      expect(result).toMatch(/[A-Z]/);
+      expect(result).toMatch(/[a-z]/);
+    });
   });
 
-  it('should 6 passwords be generated', () => {
-    const COUNT_PASSWORD = 6;
+  describe('generateMultiple', () => {
+    it('should 6 passwords be generated', () => {
+      const COUNT_PASSWORD = 6;
 
-    const passwords = password.generateMultiple(COUNT_PASSWORD);
+      const results = sut.generateMultiple(COUNT_PASSWORD);
 
-    expect(passwords).toHaveLength(COUNT_PASSWORD);
+      expect(results).toHaveLength(COUNT_PASSWORD);
+    });
   });
 });
